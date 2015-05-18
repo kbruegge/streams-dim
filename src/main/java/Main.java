a21811ff6d77058ee56aebc2ac0c90984d6bcb74
Kai MacGyver <kai@Kais-Mac-mini.local>
2015-05-18 17:14:40 +0200

    added dim source code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,17 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello");
+//        String path = System.getProperty("java.library.path");
+        System.setProperty("java.library.path", "/Users/kai/Development/dimstream/");
+        String path = System.getProperty("java.library.path");
+        System.out.println("Library path: " + path);
+
         DimInfo pointing = new DimInfo("DRIVE/DRIVE_POINTING_POSITION",5,-1);
-        System.out.println("Pointing: " + pointing.getString());
+
+        System.out.println("Loading by hand");
+//        System.loadLibrary("libjdim");
+        System.load("/Users/kai/Development/dimstream/libjdim.dylib");
+
+//        System.out.println("Pointing: " + pointing.getString());
     }
 }
